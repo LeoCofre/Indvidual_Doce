1fb0df16b11d0239932fd76ea72c106b77b1e9c7
Leonardo Cofré Oyarzún <leocofre1981@gmail.com>
2023-06-28 22:15:48 -0400

    activity_second

--- app/src/main/java/cl/awakelab/indvidualdoce/MainActivity.java
+++ app/src/main/java/cl/awakelab/indvidualdoce/MainActivity.java
@@ -2,13 +2,28 @@ package cl.awakelab.indvidualdoce;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import cl.awakelab.indvidualdoce.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding;
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.btnFood.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), SecondActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
